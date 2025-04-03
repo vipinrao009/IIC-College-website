451da2bc79d3bdb81edb3c1cddeac940ca858f9a
vipinrao009 <vipinrao2018@gmail.com>
2025-04-04 01:48:08 +0530

    feat: Add user schema with validation and password hashing

---
+++ server/model/userSchema.js
@@ -0,0 +1,47 @@
+import {Schema, model } from "mongoose"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema({
+    name: {
+        type:String,
+        unique:true,
+        required:true,
+        minLength:[3,"Username must conatain atleast 3 characters."],
+        maxLength:[40,"Username can't exceed 40 characters."]
+    },
+    password:{
+        type:String,
+        select:false,
+        required:true,
+        minLength:[8,"Username must conatain atleast 8 characters."],
+    },
+    email:{
+        type:String,
+    },
+    address:{
+        type:String
+    },
+    phone:{
+        type:String,
+        minLength:[10,"Phone number must conatin exact 10 digits."],
+        maxLength:[10,"Phone number must conatin exact 10 digits."]
+    },
+    profileImage:{
+        public_id:{
+         type:String,
+         required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+    },
+})
+
+userSchema.pre("save", async function(){
+    if(!this.isModified("password")){
+        return next()
+    }
+    this.password = await bcrypt.hash(this.password,10)
+})
+export const User = model("User",userSchema)
