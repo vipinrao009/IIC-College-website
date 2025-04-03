134a4a024d51c8b0321a50e20853e3113809f4fa
vipinrao009 <vipinrao2018@gmail.com>
2025-04-04 01:33:23 +0530

    feat: file upload with multer, and cloudinary integration

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      folder: "Auction plateform",
+      resource_type: "auto",
+    });
+
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    console.log(error)
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
