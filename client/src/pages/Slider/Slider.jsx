845c307b2761f3f7abaee7058191449fde57fe65
vipinrao009 <vipinrao2018@gmail.com>
2025-05-10 23:29:53 +0530

    fix: adjust image size and heading styles in AutoImageSlider for improved layout

--- client/src/pages/Slider/Slider.jsx
+++ client/src/pages/Slider/Slider.jsx
@@ -32,19 +32,19 @@ const AutoImageSlider = () => {
             <img
               src={image}
               alt={`slide-${index}`}
-              className="w-full h-[40vh] md:h-[85vh] object-cover"
+              className="w-full h-[50vh] md:h-[85vh] object-cover"
             />
             {/* Overlay */}
             <div className="absolute inset-0 bg-black/50 flex items-center justify-center">
               <div className="text-center text-white px-6 max-w-2xl animate-fadeIn">
-                <h2 className="text-4xl md:text-5xl font-extrabold tracking-wide mb-4 animate-fadeInUp">
+                <h2 className="text-2xl md:text-5xl font-extrabold tracking-wide mb-4 animate-fadeInUp">
                   OUR <span className="text-yellow-400 drop-shadow-md">MISSION</span>
                 </h2>
-                <p className="text-lg md:text-xl leading-relaxed text-gray-200 animate-fadeIn delay-300">
+                <p className=" md:text-xl leading-relaxed text-gray-200 animate-fadeIn delay-300">
                   We always provide our best educational services to empower and uplift every student towards excellence.
                 </p>
                 <div className="mt-6">
-                  <button onClick={() => navigate("/about")}  className="px-6 py-3 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold rounded-full shadow-lg transition duration-300">
+                  <button onClick={() => navigate("/about")}  className="px-5 py-2 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold rounded-full shadow-lg transition duration-300">
                     Learn More
                   </button>
                 </div>
